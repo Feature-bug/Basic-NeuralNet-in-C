26f11477d0631db6e86f15ba98198377ef2d1654
Feature-bug <sidharthmenon1212@gmail.com>
2026-08-29 13:21:03 +0530

    cleaned up struct, not much was done

--- neural.c
+++ neural.c
@@ -19,40 +19,47 @@ OK I want to make a neural net in C so let me get a check list
 * Output I beleive would be straight forward, It would be the value the last neuron would give after passing throught the activation function.
 * Backprop, ok this I have no fuckin clue on how to do I know it uses the chain rule, how the fuck am i supposed to implement that in code! I guess I could apply a
   limit of sorts I believe the formula was ~~f(h) = (f(x) + f(x + h)) / h~~, but this is off the top of my head so I could be wrong (It is wrong).
-  Update: I just the formula was wrong it was f(x) = (f(x + h) - f(x)) / h, where h tends to infinity.
+  Update: the formula was wrong it was f(x) = (f(x + h) - f(x)) / h, where h tends to infinity.
 */
 
 //A neuron contains its weight, bias and the number of inputs going into it.
 typedef struct{
     double *weight;
-    double bias;
+    double *bias;
+    double *z;
+    double *activated;
+    double *delta;
     int NumberofInput;
-}neuron;
+    int NumberofOutput;
+}Layer;
+
+typedef struct{
+    int NumberofLayer;
+    Layer *layers;
+}NeuralNet;
 
 //Sigmoid activation function, straight forward to implement.
 double sigmoid(double out){
     return 1.0 / (1.0 + exp(-out));
 }
 
-double mean_square_error(neuron *out_pred, double *out_real, int NumberofInput){
+double mean_square_error(double *out_pred, double *out_real, int NumberofInput){
     double error = 0.0;
     for(int i = 0; i < NumberofInput; i++){
-        error += pow(out_pred->weight[i] - out_real[i], 2);
+        error += pow(out_pred[i] - out_real[i], 2);
     }
     return error / NumberofInput;
 }
 
-double regression(neuron *neuron, float *input, int NumberofInput){
+double regression(Layer *neuron, float *input, int NumberofInput){
     double out = 0.0;
     for(int i = 0; i < NumberofInput; i++){
-        out += neuron->weight[i] * input[i];
+        out += neuron->weight[i] * input[i] + neuron->bias[i];
     }
-    return out + neuron->bias;
+    return out;
 }
 
-double gradient_descent(neuron *neuron, double error, int index){
-    long alpha = 0.01;
-    neuron->weight[index] -= alpha * error;
+double gradient_descent(Layer *neuron, double error){
     //code
 }
 
